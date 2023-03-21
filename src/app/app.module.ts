3ba82dbb3eb3d4c656ce0032836269d0a50ff188
Victor Barboza <63432907+MercurialBR@users.noreply.github.com>
2023-03-21 11:35:39 -0300

    Initial commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { TopoComponent } from './components/topo/topo.component';
+import { RodapeComponent } from './components/rodape/rodape.component';
+import { CorpoComponent } from './components/corpo/corpo.component';
+import { HomeComponent } from './home/home.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    TopoComponent,
+    RodapeComponent,
+    CorpoComponent,
+    HomeComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
