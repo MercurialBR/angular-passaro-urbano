9e5b12f8555ae4b0377665e7ca5e342f868b71de
Victor Barboza <63432907+MercurialBR@users.noreply.github.com>
2023-03-23 17:32:35 -0300

    aprimorando services e promises

--- src/app/components/corpo/corpo.component.ts
+++ src/app/components/corpo/corpo.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import { OfertasService } from 'src/app/ofertas.service';
+import { Oferta } from 'src/app/shared/oferta.model';
 
 @Component({
   selector: 'app-corpo',
   templateUrl: './corpo.component.html',
-  styleUrls: ['./corpo.component.css']
+  styleUrls: ['./corpo.component.css'],
+  providers: [OfertasService]
 })
-export class CorpoComponent {
+export class CorpoComponent implements OnInit {
 
+  public ofertas!: Oferta[]
+
+  constructor(private ofertasService: OfertasService) { }
+
+  ngOnInit() {
+    //  this. ofertas = this.ofertasService.getOfertas()
+
+    this.ofertasService.getOfertas2()
+      .then((ofertas: Oferta[]) => {
+        console.log('depois de 3seg')
+        this.ofertas = ofertas
+      })
+      .catch((param: any) => {
+        console.log(param)
+      })
+
+  }
 }
