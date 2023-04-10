564478013eae0b8eb2589209ad99880ccb6c8541
Victor Barboza <63432907+MercurialBR@users.noreply.github.com>
2023-04-10 20:55:37 -0300

    atualizando

--- src/app/oferta/oferta.component.ts
+++ src/app/oferta/oferta.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { OfertasService } from '../ofertas.service';
 import { Oferta } from '../shared/oferta.model';
+import { Observable, Subscriber, interval } from 'rxjs';
 
 @Component({
   selector: 'app-oferta',
@@ -24,5 +25,11 @@ export class OfertaComponent implements OnInit{
     .then((oferta: Oferta) => {
       this.oferta = oferta
     })
+
+  let tempo = interval(2000)
+
+  tempo.subscribe((intervalo: number) => {
+    console.log(intervalo)
+  })
   }
 }
